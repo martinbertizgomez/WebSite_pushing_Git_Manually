d91c592fc833c6e1ee4b4c4d6f05e20780fa8d90
Martin <martinbertizgomez@outlook.com>
2023-01-26 12:31:44 +0100

    lots of files

---
+++ WebSite_pushing_Git_Manually/App_Code/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Microsoft.AspNet.FriendlyUrls;
+
+namespace WebSite_pushing_Git_Manually
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            var settings = new FriendlyUrlSettings();
+            settings.AutoRedirectMode = RedirectMode.Permanent;
+            routes.EnableFriendlyUrls(settings);
+        }
+    }
+}
---
+++ WebSite_pushing_Git_Manually/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebSite_pushing_Git_Manually.Startup))]
+namespace WebSite_pushing_Git_Manually
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
